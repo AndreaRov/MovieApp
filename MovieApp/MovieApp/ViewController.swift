0d4716a51845e414cc4e12a395b224b8a1619fa9
andrea <andrea@andreas-macbook-pro.local>
2018-11-06 01:14:09 +0100

    Project created

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by andrea on 06/11/2018.
+//  Copyright © 2018 andrea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
